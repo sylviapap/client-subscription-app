d05ee1a26ccc0e9ad1485d4e718d95632ea486c4
Sylvia <sylviapap1@gmail.com>
2020-04-14 01:39:35 -0700

    created signup

--- src/services/api.js
+++ src/services/api.js
@@ -8,32 +8,41 @@ const headers = {
   Authorization: token
 };
 
-const getSubscriptions = async () => {
-  const res = await fetch(`${API_ROOT}/subscriptions/`, { headers: headers });
-    return await res.json();
+const getSubscriptions = () => {
+  fetch(`${API_ROOT}/subscriptions/`, { headers: headers })
+  .then(res => res.json())};
+
+const signup = (username, password) => {
+  fetch(`${API_ROOT}/users/`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json',
+    Accept: 'application/json'},
+    body: JSON.stringify({ username, password })
+})
+  .then(res => res.json())
 };
 
-const login = async (username, password) => {
-  const res = await fetch(`${API_ROOT}/auth/`, {
+const login = (username, password) => {
+  fetch(`${API_ROOT}/auth/`, {
         method: 'POST',
         headers: headers,
         body: JSON.stringify({ username, password })
-    });
-    return await res.json();
+    })
+    .then(res => res.json());
 };
 
-const getCurrentUser = async () => {
-  const res = await fetch(`${API_ROOT}/current_user`, {
+const getCurrentUser = () => {
+  fetch(`${API_ROOT}/current_user`, {
         headers: headers
-    });
-    return await res.json();
+    })
+    .then(res => res.json())
 };
 
 export default {
   auth: {
     login: login,
-    getCurrentUser: getCurrentUser
-  },
+    getCurrentUser: getCurrentUser,
+    signup: signup},
   subscriptions: {
     getSubscriptions
   }
