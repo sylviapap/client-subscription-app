55c77cad4a016344f3d859f1e3c2a1f2b58f3693
Sylvia <sylviapap1@gmail.com>
2020-04-14 21:50:25 -0700

    moved subs to container

--- src/App.js
+++ src/App.js
@@ -3,51 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import AppContainer from './containers/AppContainer'
 import LoginContainer from './containers/LoginContainer';
-import SignUp from './components/SignUp'
 
 class App extends Component {
 
   state = {
-    company: "",
-    cost: 0,
     auth: { 
       currentUser: {} 
     }
   }
-
-  handleSubscriptionSubmit = event => {
-    event.preventDefault()
-    
-    fetch("http://localhost:3001/api/v1/subscriptions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        company: this.state.company,
-        cost: this.state.cost
-      })
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Successful post');
-    })
-    .catch((error) => {
-      console.error('Error while post');
-    });
-  }
-
-  handleName = event => {
-    this.setState({
-        company: event.target.value
-    })
-  }
-
-  handleCost = event => {
-    this.setState({
-        cost: event.target.value
-    })
-  } 
 
   setUser = (user) => {
     this.setState({ auth: { currentUser: user } })
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -3,6 +3,45 @@ import NavBar from '../components/NavBar'
 import SubsForm from '../components/SubsForm'
 
 class AppContainer extends React.Component {
+
+    state = {
+        company: "",
+        cost: 0
+      }
+
+    handleSubscriptionSubmit = event => {
+        event.preventDefault()
+        
+        fetch("http://localhost:3001/api/v1/subscriptions", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            company: this.state.company,
+            cost: this.state.cost
+          })
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Successful post');
+        })
+        .catch((error) => {
+          console.error('Error while post');
+        });
+      }
+    
+      handleName = event => {
+        this.setState({
+            company: event.target.value
+        })
+      }
+    
+      handleCost = event => {
+        this.setState({
+            cost: event.target.value
+        })
+      } 
     
     render() {
     return (  
